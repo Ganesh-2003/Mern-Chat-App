27bb5f057db298d057b26f849c7b3f57c41fd3a5
Ganesh-2003 <ganeshvsp12@gmail.com>
2025-04-06 03:06:47 +0530

    Intial Commit

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,47 @@
 const express = require("express");
+const dotenv  = require("dotenv")
+const chats= require("./data/data");
+
 
 const app = express();
+dotenv.config();
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
 
     res.status(200).json({
-        msg: "API is running",
+        msg: "API is running Successfully",
     });
     
 });
 
-app.listen(3000, console.log("Server Started at PORT:3000"));
+app.get("/api/chat", (req,res) =>{
+
+    res.send(chats);
+
+})
+
+app.get("/api/chat/:id", (req,res) => {
+
+    const id = req.params.id;
+    //const singleChat = chats.find((c) => c._id === id) // Potential way to find id
+    
+    for (let i = 0;i<chats.length; i++) {
+        if(chats[i]._id === id) {
+            const singleChat = chats[i];
+            res.send(singleChat);
+        }
+    }
+    
+    res.send(404).json({
+        msg: "id value is wrong Chat is not found"
+    })
+    
+})
+
+const PORT = process.env.PORT ||  5000
+
+app.listen(PORT ,()=>{
+    console.log(`Server Started in Port: ${PORT}`);
+});
