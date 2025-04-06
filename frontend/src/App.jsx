90e3fb2b3759cb94c2eaabf86da014286def050f
Ganesh-2003 <ganeshvsp12@gmail.com>
2025-04-06 14:43:46 +0530

    Intial Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Button } from "@chakra-ui/react"
 import { BrowserRouter as Router, Routes, Route } from "react-router"
 import HomePage from './pages/HomePage.jsx'
 import ChatPage from './pages/ChatPage.jsx'
