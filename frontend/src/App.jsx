f37a286309c1c40965a337affaff346513da3184
Ganesh-2003 <ganeshvsp12@gmail.com>
2025-04-06 16:59:29 +0530

    Intial Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-
       <div>
         <Routes>
           <Route path="/" element={<HomePage />} />
@@ -15,7 +14,6 @@ function App() {
         </Routes>
       
       </div>
-
   );
 }
 
